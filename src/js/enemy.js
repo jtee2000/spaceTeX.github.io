88b0c45b6bd91f3e341e856054048de12081f473
Jason Tee <teej@bc.edu>
2019-08-14 16:44:08 -0400

    Add alien

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -1,7 +1,7 @@
 export default class Enemey {
 
     constructor() {
-        this.createAsteroid = this.createAsteroid.bind(this); 
+        this.createAsteroid = this.drawAsteroid.bind(this); 
         this.animateAsteroid = this.animateAsteroid.bind(this);
         this.canvas = document.getElementById("splash");
         this.canvas.width = 1600; 
@@ -9,39 +9,59 @@ export default class Enemey {
         this.ctx = this.canvas.getContext("2d");
         this.x = 0; 
         this.y = 0; 
+        this.x_alien = 800; 
+        this.y_alien = 800; 
         this.dx = 2; 
         this.dy = -2; 
+        this.dx_alien = -3; 
+        this.dy_alien = 3; 
 
         setInterval(this.animateAsteroid, 10);
     }
     
     
-    createAsteroid() {
+    drawAsteroid() {
         // debugger
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         const asteroid = new Image(); 
+        const alien = new Image(); 
         // asteroid.src = "https://latex-racer.s3.amazonaws.com/asteroid.png";
-        asteroid.src = "src/assets/asteroid.png"
+        asteroid.src = "src/assets/asteroid.png"; 
+        alien.src = "src/assets/silverufo.png"
         asteroid.onload = () => {
             // this.ctx.imageSmoothingEnabled = true; 
             // this.ctx.drawImage(asteroid, 46, 49, 210, 190, this.x, this.y, 64, 70.73);
             // this.ctx.drawImage(asteroid, 46, 49, 210, 190, this.x, this.y, 64, 70.73);
             this.ctx.drawImage(asteroid, this.x, this.y)  
         };
+        alien.onload = ( () => {
+            this.ctx.drawImage(alien, this.x_alien, this.y_alien, 250, 250)
+        })
+
     }
+
+    
 
     animateAsteroid() {
         debugger
         this.x += this.dx; 
         this.y += this.dy; 
-        if (this.x > this.canvas.width || this. x < 0) {
+        this.x_alien += this.dx_alien; 
+        this.y_alien += this.dy_alien; 
+        if (this.x > this.canvas.width -256 || this.x < 0) {
             debugger
             this.dx = -this.dx; 
         }
-        if (this.y > this.canvas.height || this.y < 0) {
+        if (this.y > this.canvas.height- 256 || this.y < 0) {
             this.dy = -this.dy; 
         }
-        this.createAsteroid(); 
+        if (this.x_alien > this.canvas.width || this.x_alien < 0) {
+            this.dx_alien = -this.dx_alien
+        }
+        if (this.y_alien > this.canvas.height || this.y_alien < 0) {
+            this.dy_alien = -this.dy_alien
+        }
+        this.drawAsteroid(); 
     }
 
 
