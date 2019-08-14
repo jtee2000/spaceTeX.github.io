a3196077a7c847d373babbd77b9a0a1895c9d6d9
Jason Tee <teej@bc.edu>
2019-08-14 16:25:21 -0400

    Animate astroid

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -2,22 +2,49 @@ export default class Enemey {
 
     constructor() {
         this.createAsteroid = this.createAsteroid.bind(this); 
-        this.createAsteroid(); 
-        const canvas = document.getElementById("splash");
-        this.ctx = canvas.getContext("2d");
+        this.animateAsteroid = this.animateAsteroid.bind(this);
+        this.canvas = document.getElementById("splash");
+        this.canvas.width = 1600; 
+        this.canvas.height = 800; 
+        this.ctx = this.canvas.getContext("2d");
+        this.x = 0; 
+        this.y = 0; 
+        this.dx = 2; 
+        this.dy = -2; 
+
+        setInterval(this.animateAsteroid, 10);
     }
     
     
     createAsteroid() {
+        // debugger
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         const asteroid = new Image(); 
         // asteroid.src = "https://latex-racer.s3.amazonaws.com/asteroid.png";
         asteroid.src = "src/assets/asteroid.png"
         asteroid.onload = () => {
-            this.ctx.imageSmoothingEnabled = true; 
-            // this.ctx.drawImage(asteroid, 0, 0, 256, 256, 10, 10, 256 / 2, 256 / 2);
-            this.ctx.drawImage(asteroid, 0, 0, asteroid.width, asteroid.height)  
+            // this.ctx.imageSmoothingEnabled = true; 
+            // this.ctx.drawImage(asteroid, 46, 49, 210, 190, this.x, this.y, 64, 70.73);
+            // this.ctx.drawImage(asteroid, 46, 49, 210, 190, this.x, this.y, 64, 70.73);
+            this.ctx.drawImage(asteroid, this.x, this.y)  
         };
     }
+
+    animateAsteroid() {
+        debugger
+        this.x += this.dx; 
+        this.y += this.dy; 
+        if (this.x > this.canvas.width || this. x < 0) {
+            debugger
+            this.dx = -this.dx; 
+        }
+        if (this.y > this.canvas.height || this.y < 0) {
+            this.dy = -this.dy; 
+        }
+        this.createAsteroid(); 
+    }
+
+
 
 
 }
--- src/js/layout.js
+++ src/js/layout.js
@@ -6,6 +6,8 @@ export default class Layout {
 
     makeLayout() {
         const canvas = document.getElementById("splash");
+        canvas.width = 1600; 
+        canvas.height = 800; 
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(150, 75, 2, 0, 2 * Math.PI);
